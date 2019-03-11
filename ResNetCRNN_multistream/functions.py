83d07d99eae86b95bb11b472b21f1d3b019ffea9
Huan-Hsin Tseng <htseng@2379590988.vpn.umich.net>
2019-03-11 02:47:10 -0400

    init

---
+++ ResNetCRNN_multistream/functions.py
@@ -0,0 +1,174 @@
+import os
+import numpy as np
+from PIL import Image
+from torch.utils import data
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.models as models
+import torchvision.transforms as transforms
+from tqdm import tqdm
+
+## ------------------- label conversion tools ------------------ ##
+def labels2cat(label_encoder, list):
+    return label_encoder.transform(list)
+
+def labels2onehot(OneHotEncoder, label_encoder, list):
+    return OneHotEncoder.transform(label_encoder.transform(list).reshape(-1, 1)).toarray()
+
+def onehot2labels(label_encoder, y_onehot):
+    return label_encoder.inverse_transform(np.where(y_onehot == 1)[1]).tolist()
+
+def cat2labels(label_encoder, y_cat):
+    return label_encoder.inverse_transform(y_cat).tolist()
+
+
+## ---------------------- Dataloader ---------------------- ##
+# for CRNN multistream
+class Dataset_CRNN_multistream(data.Dataset):
+    "Characterizes a dataset for PyTorch"
+    def __init__(self, data_path, folders, labels, frames, transform=None):
+        "Initialization"
+        self.data_path = data_path
+        self.labels = labels
+        self.folders = folders
+        self.transform = transform
+        self.frames = frames
+
+    def __len__(self):
+        "Denotes the total number of samples"
+        return len(self.folders)
+
+    def read_images(self, path, selected_folder, use_transform):
+        X = []
+        for i in self.frames:
+            image = Image.open(os.path.join(path, selected_folder, 'frame{:06d}.jpg'.format(i)))
+
+            if use_transform is not None:
+                image = use_transform(image)
+
+            X.append(image)
+        X = torch.stack(X, dim=0)
+
+        return X
+
+    def __getitem__(self, index):
+        "Generates one sample of data"
+        # Select sample
+        folder = self.folders[index]
+
+        # Load data
+        X1 = self.read_images(self.data_path['spatial'], folder, self.transform)   # spatial stream
+        X2 = np.tile(self.read_images(self.data_path['motion_x'], folder, self.transform), (1, 3, 1, 1))  # motion_x stream: gray -> RGB
+        X3 = np.tile(self.read_images(self.data_path['motion_y'], folder, self.transform), (1, 3, 1, 1))  # motion_y stream: gray -> RGB
+
+        y = torch.LongTensor([self.labels[index]])  # (labels) LongTensor are for int64 instead of FloatTensor
+
+        return [X1, X2, X3], y
+## ---------------------- end of Dataloader ---------------------- ##
+
+
+## -------------------- (reload) model prediction ---------------------- ##
+def CRNN_final_prediction_multistream(model, device, loader):
+    spatial_encoder, motion_encoder, rnn_decoder = model
+    spatial_encoder.eval()
+    motion_encoder.eval()
+    rnn_decoder.eval()
+
+    all_y_pred = []
+    with torch.no_grad():
+        for batch_idx, ([X1, X2, X3], y) in enumerate(tqdm(loader)):
+            # distribute data to device
+            X1, X2, X3 = X1.to(device), X2.to(device), X3.to(device)
+            X1, X2, X3 = spatial_encoder(X1), motion_encoder(X2), motion_encoder(X3)
+            X = torch.cat((X1, X2, X3), dim=2)       # concat all streams
+            output = rnn_decoder(X)                  # output has dim = (batch, number of classes)
+            y_pred = output.max(1, keepdim=True)[1]  # location of max log-probability as prediction
+            all_y_pred.extend(y_pred.cpu().data.squeeze().numpy().tolist())
+
+    return all_y_pred
+## -------------------- end of model prediction ---------------------- ##
+
+
+
+## ------------------------ ResNet CNN module ---------------------- ##
+# 2D CNN encoder using ResNet-152 pretrained
+class ResCNNEncoder(nn.Module):
+    def __init__(self, fc_hidden1=512, fc_hidden2=512, drop_p=0.3, CNN_embed_dim=300):
+        """Load the pretrained ResNet-152 and replace top fc layer."""
+        super(ResCNNEncoder, self).__init__()
+
+        self.fc_hidden1, self.fc_hidden2 = fc_hidden1, fc_hidden2
+        self.drop_p = drop_p
+
+        resnet = models.resnet152(pretrained=True)
+        modules = list(resnet.children())[:-1]      # delete the last fc layer.
+        self.resnet = nn.Sequential(*modules)
+        self.fc1 = nn.Linear(resnet.fc.in_features, fc_hidden1)
+        self.bn1 = nn.BatchNorm1d(fc_hidden1, momentum=0.01)
+        self.fc2 = nn.Linear(fc_hidden1, fc_hidden2)
+        self.bn2 = nn.BatchNorm1d(fc_hidden2, momentum=0.01)
+        self.fc3 = nn.Linear(fc_hidden2, CNN_embed_dim)
+        
+    def forward(self, x_3d):
+        cnn_embed_seq = []
+        for t in range(x_3d.size(1)):
+            # ResNet CNN
+            with torch.no_grad():
+                x = self.resnet(x_3d[:, t, :, :, :])  # ResNet
+                x = x.view(x.size(0), -1)             # flatten output of conv
+
+            # FC layers
+            x = self.bn1(self.fc1(x))
+            x = F.relu(x)
+            x = self.bn2(self.fc2(x))
+            x = F.relu(x)
+            x = F.dropout(x, p=self.drop_p, training=self.training)
+            x = self.fc3(x)
+
+            cnn_embed_seq.append(x)
+
+        # swap time and sample dim such that (sample dim, time dim, CNN latent dim)
+        cnn_embed_seq = torch.stack(cnn_embed_seq, dim=0).transpose_(0, 1)
+        # cnn_embed_seq: shape=(batch, time_step, input_size)
+
+        return cnn_embed_seq
+
+
+class DecoderRNN(nn.Module):
+    def __init__(self, CNN_embed_dim=300, h_RNN_layers=3, h_RNN=256, h_FC_dim=128, drop_p=0.3, num_classes=50):
+        super(DecoderRNN, self).__init__()
+
+        self.RNN_input_size = CNN_embed_dim
+        self.h_RNN_layers = h_RNN_layers   # RNN hidden layers
+        self.h_RNN = h_RNN                 # RNN hidden nodes
+        self.h_FC_dim = h_FC_dim
+        self.drop_p = drop_p
+        self.num_classes = num_classes
+
+        self.LSTM = nn.LSTM(
+            input_size=self.RNN_input_size,
+            hidden_size=self.h_RNN,        
+            num_layers=h_RNN_layers,       
+            batch_first=True,       # input & output will has batch size as 1s dimension. e.g. (batch, time_step, input_size)
+        )
+
+        self.fc1 = nn.Linear(self.h_RNN, self.h_FC_dim)
+        self.fc2 = nn.Linear(self.h_FC_dim, self.num_classes)
+
+    def forward(self, x_RNN):
+        
+        self.LSTM.flatten_parameters()
+        RNN_out, (h_n, h_c) = self.LSTM(x_RNN, None)  
+        """ h_n shape (n_layers, batch, hidden_size), h_c shape (n_layers, batch, hidden_size) """ 
+        """ None represents zero initial hidden state. RNN_out has shape=(batch, time_step, output_size) """
+
+        # FC layers
+        x = self.fc1(RNN_out[:, -1, :])   # choose RNN_out at the last time step
+        x = F.relu(x)
+        x = F.dropout(x, p=self.drop_p, training=self.training)
+        x = self.fc2(x)
+
+        return x
+## ---------------------- end of CRNN module ---------------------- ##
+
